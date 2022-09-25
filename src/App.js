a73e9da00fe09bf01490a8b1d536e2075d0e64ca
Paul Duszak <duszak.paul@gmail.com>
2022-09-25 18:34:10 -0500

    week 2

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import UserBar from "./user/UserBar";
+import PostList from "./post/PostList"
+
+function App() {
+
+  const posts = [
+    {
+      title: "My first post",
+      content: "Some content",
+      author: "Paul"
+    },
+    {
+      title: "My second post",
+      content: "Some content",
+      author: "Paul"
+    }
+  ]
+
+    return (
+      <div>
+        <UserBar />
+        <PostList posts={posts} />
+      </div>
+    )       
+}
+
+
+
+
+export default App;
---
+++ src/user/Login.js
@@ -0,0 +1,13 @@
+function Login() {
+    return (
+        <form onSubmit={e => e.preventDefault()}>
+            <label htmlFor="login-username">Username:</label>
+            <input type="text" name="login-username" id="login-username" />
+            <label htmlFor="login-password">Password:</label>
+            <input type="password" name="login-password" id="login-password" />
+            <input type="submit" value="Login" />
+        </form>
+    )
+}
+
+export default Login;
